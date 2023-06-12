dac09ebd5579d1152fedf4b097a4c4b3f2403869
Альберт Каримов <chopickzzz@gmail.com>
2023-06-12 04:33:48 +0300

    widgets: Navbar

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,30 @@
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <div className={classNames(cls.links)}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={"/"}
+          className={cls.mainLink}
+        >
+          Домой
+        </AppLink>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={"/about"}
+          className={cls.aboutLink}
+        >
+          ABOUT
+        </AppLink>
+      </div>
+    </div>
+  );
+};
