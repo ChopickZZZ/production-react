01a37e3a42d6e96a0bbf3be3d7079b8b252b4734
Альберт Каримов <chopickzzz@gmail.com>
2023-06-23 18:24:01 +0300

    stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: "stylelint-config-standard-scss",
+  plugins: ["stylelint-scss"],
+  rules: {
+    "selector-class-pattern": null,
+    "value-keyword-case": null,
+    "custom-property-empty-line-before": null,
+    "no-empty-source": null,
+  },
+};
