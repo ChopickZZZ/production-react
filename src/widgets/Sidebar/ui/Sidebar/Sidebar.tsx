c6625a7512a57142d66dc2c44f0ce71e9dc09193
Альберт Каримов <chopickzzz@gmail.com>
2024-09-09 00:08:17 +0300

    upgrade sidebar, add home and about icons and translations

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -10,6 +10,9 @@ import {
   ButtonSize,
   ButtonTheme,
 } from "shared/ui/AppButton/AppButton";
+import MainIcon from "shared/assets/icons/home.svg";
+import AboutIcon from "shared/assets/icons/about.svg";
+import { useTranslation } from "react-i18next";
 
 interface SidebarProps {
   className?: string;
@@ -21,6 +24,8 @@ export const Sidebar = ({ className }: SidebarProps) => {
   const onToggle = () => {
     setCollapsed((prev) => !prev);
   };
+
+  const { t } = useTranslation();
 
   return (
     <aside
@@ -40,20 +45,18 @@ export const Sidebar = ({ className }: SidebarProps) => {
         {collapsed ? ">" : "<"}
       </AppButton>
       <div className={cls.items}>
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          to={routePath.main}
-          className={cls.mainLink}
-        >
-          Домой
-        </AppLink>
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          to={routePath.about}
-          className={cls.aboutLink}
-        >
-          About us
-        </AppLink>
+        <div className={cls.item}>
+          <AppLink theme={AppLinkTheme.SECONDARY} to={routePath.main}>
+            <MainIcon className={cls.icon} />
+            <span className={cls.link}>{t("Главная")}</span>
+          </AppLink>
+        </div>
+        <div className={cls.item}>
+          <AppLink theme={AppLinkTheme.SECONDARY} to={routePath.about}>
+            <AboutIcon className={cls.icon} />
+            <span className={cls.link}>{t("О сайте")}</span>
+          </AppLink>
+        </div>
       </div>
       <div className={cls.switchers}>
         <ThemeSwitcher />
